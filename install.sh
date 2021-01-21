63c2483ae0bf2a01c67ecb74a05a705313268d0d
Julio Torres <jtorres990@gmail.com>
2021-01-21 17:35:54 -0500

    feat(install): se añade fichero de configuracion de ejemplo

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ $USER != "root" ]; then
-  echo "Eres root"
+  echo "No eres root"
   exit 1
 fi
 
 systemctl stop proxy-logger > /dev/null
 cp bin/proxy-logger /usr/bin/proxy-logger
 mkdir -p /etc/proxy-logger
-cp config.yml /etc/proxy-logger/config.yml
+cp config.yml /etc/proxy-logger &> /dev/null
+cp config.yml.example /etc/proxy-logger &> /dev/null
 cp proxy-logger.service /etc/systemd/system
 adduser proxy-logger \
     --gecos "" \
